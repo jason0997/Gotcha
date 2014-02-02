fd676c7c34db2682d342b8b5060ec4ccfc781518
jason0997 <417703557@qq.com>
2014-02-02 15:06:35 -0500

    Score done. Random Color and random number fixed.

--- javascripts/helper.js
+++
@@ -1,9 +0,0 @@
-//Create a random number
-function randomNumber(range){
-	return Math.floor(Math.random() * range);
-}
-function getColor(){
-	var colorArray = new Array ("white", "green", "blue", "red", "yellow", "Cyan", "pink" , "purple", "Brown");
-	var colorIndex = Math.floor(Math.random() * colorArray.length);	
-	return colorArray[colorIndex];	
-}
